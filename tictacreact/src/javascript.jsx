576f0534c2c82d3e24952e2e53feb1cc355e44a6
Jesse Smith <JesseSmith3rd@outlook.com>
2018-03-20 22:32:41 -0400

    adjust file name, adjust logic

--- tictacreact/src/javascript.jsx
+++ tictacreact/src/javascript.jsx
@@ -1,8 +1,14 @@
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({value:'X'})}>
+        {this.state.value}
       </button>
     );
   }
@@ -10,7 +16,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
